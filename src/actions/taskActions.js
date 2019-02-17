3f6a8d73306349fb10cb315059f433f8ebd620cf
xuatz <xzlow10@gmail.com>
2019-02-17 18:12:56 +0900

    enhance task action to take in task object for future-proofing

--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -1,17 +1,30 @@
 import uuid from "uuid/v4";
 import PouchDB from "pouchdb";
 
-export const taskAdd = title => {
-    return (dispatch, getState) => {
-        dispatch({ type: "TASK_ADD", task: { id: uuid(), title } });
-        let state = getState();
-        return Promise.resolve({ status: 200 });
+export const taskAdd = task => {
+  return (dispatch, getState) => {
+    const now = new Date();
+    const newTask = {
+      id: uuid(),
+      userId: undefined, // TODO
+      createdAt: now.getTime() / 1000,
+      createdBy: undefined, // TODO
+      modifiedAt: now.getTime() / 1000,
+      modifiedBy: undefined, // TODO
+      dueDate: undefined, // TODO
+      category: undefined, // TODO
+      tags: undefined, // TODO
+      ...task
     };
+    dispatch({ type: "TASK_ADD", task: newTask });
+    let state = getState();
+    return Promise.resolve({ status: 200 });
+  };
 };
 
 export const taskDelete = id => {
-    return dispatch => {
-        dispatch({ type: "TASK_DELETE", id });
-        return Promise.resolve({ status: 200 });
-    };
+  return dispatch => {
+    dispatch({ type: "TASK_DELETE", id });
+    return Promise.resolve({ status: 200 });
+  };
 };
