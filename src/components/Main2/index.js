971907b9193d65d5d845d31465a8e76515d0e010
xuatz <xzlow10@gmail.com>
2019-07-30 22:49:03 +0900

    ???

--- src/components/Main2/index.js
+++ src/components/Main2/index.js
@@ -1,27 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 
-const TaskInput = props => <div css={props.css}>TaskInput</div>;
+const TaskInput = props => {
+  const [title, setTitle] = useState('');
 
-const TaskList = props => <div css={props.css}>TaskList</div>;
+  return (
+    <div css={props.style}>
+      <form
+        style={{
+          padding: '30px 20px',
+          backgroundColor: 'red',
+          display: 'flex',
+        }}
+        onSubmit={e => {
+          e.preventDefault();
+          if (title && title != '') {
+            // props.taskAdd(title);
+            setTitle('');
+          }
+        }}>
+        <input
+          style={{
+            flex: 1,
+            backgroundColor: 'grey',
+            fontSize: '1.5em',
+            padding: '10px 0px',
+          }}
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+};
 
-const TaskDetail = props => <div css={props.css}>TaskDetail</div>;
+const TaskList = props => <div css={props.style}>TaskList</div>;
+
+const TaskDetail = props => <div css={props.style}>TaskDetail</div>;
 
 const styles = {
   mainContainer: css`
     display: flex;
-    flex-direction: 'column';
-    justify-items: center;
+    flex-direction: column;
+    align-items: stretch;
+    padding: 20px;
 
     @media (min-width: 480px) {
-      flex-direction: 'row';
+      flex-direction: row;
+      align-items: flex-start;
       justify-items: left;
     }
   `,
   leftContainer: css`
+    flex: 1;
     display: flex;
     flex-direction: column;
+  `,
+  rightContainer: css`
+    background-color: green;
+    flex: 1;
+
+    @media (min-width: 480px) {
+      flex: 2;
+    }
   `,
   taskInput: css`
     flex: 2;
@@ -35,10 +77,10 @@ const Main2 = props => {
   return (
     <div css={styles.mainContainer}>
       <div css={styles.leftContainer}>
-        <TaskInput css={styles.taskInput} />
-        <TaskList css={styles.taskList} />
+        <TaskInput style={styles.taskInput} />
+        <TaskList style={styles.taskList} />
       </div>
-      <TaskDetail />
+      <TaskDetail style={styles.rightContainer} />
     </div>
   );
 };
